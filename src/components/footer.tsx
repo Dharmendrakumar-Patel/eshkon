c88c90ceaa5f6e37cbb6335c4e93339a2088ece3
Dharmendrakumar Patel <dharmendrakumar.patel@hotmail.com>
2024-02-26 18:24:22 +0530

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="w-full bg-[#f2f2ef] border-gray-200 dark:bg-gray-900 flex items-center justify-center py-3">
         <p>
-            <span className='mr-2'>Createb By</span>
+            <span className='mr-2'>Created By</span>
             <Link href="https://www.linkedin.com/in/dharmendrakumar-s-patel/" className='text-lg font-semibold'>
                 Dharmendrakumar Patel
             </Link>  
